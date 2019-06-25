df6b7cc0ce3cc2da42c72f9dcc15ce6da4abbc22
sidfate <15757176627@163.com>
2019-06-25 10:58:21 +0800

    :zap: publish shell.

--- auto_preview.sh
+++ auto_preview.sh
@@ -4,8 +4,8 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo $SHELL_FOLDER
 PREVIEW_PATH=$SHELL_FOLDER/dist/
 
-yarn build:prod
-cd PREVIEW_PATH
+yarn build:stage
+cd $PREVIEW_PATH
 git add ./
 git commit -m "release-autocomplete"
 git push origin master
