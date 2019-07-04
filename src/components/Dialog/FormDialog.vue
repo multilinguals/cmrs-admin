38d0e85fec732d2f8d2b19969544881cfd994124
sidfate <15757176627@163.com>
2019-07-04 22:23:37 +0800

    :hammer: basic form dialog component.

---
+++ src/components/Dialog/FormDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-dialog :title="title" :visible.sync="dialog">
+    <el-form ref="dataForm" :rules="rules" :model="form" label-position="left" :label-width="formLabelWidth"
+             style="width: 400px; margin-left:50px;">
+      <slot name="form"></slot>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialog = false">
+        取消
+      </el-button>
+      <el-button
+              type="primary"
+              @click="submitForm"
+              :loading="btnLoading"
+      >
+        确认
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    name: "FormDialog",
+    props: {
+      title: String,
+      form: Object,
+      rules: Object,
+      formLabelWidth: {
+        type: [Number, String],
+        default: '90px'
+      }
+    },
+    data() {
+      return {
+        dialog: false,
+        btnLoading: false
+      }
+    },
+    methods: {
+      submitForm() {
+        console.log('form-dialog')
+        this.btnLoading = true
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$emit('submit-form', this.form)
+          } else {
+            this.btnLoading = false
+            return false
+          }
+        })
+      },
+      close() {
+        this.btnLoading = false
+        this.dialog = false
+      },
+      open() {
+        this.btnLoading = false
+        this.dialog = true
+
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
