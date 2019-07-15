7b32b1a5d09adaa6832226b1b9c4823f7230e7d5
sidfate <15757176627@163.com>
2019-07-15 11:20:17 +0800

    :sparkles: some new in restaurant detail.

---
+++ src/views/restaurant/component/index.js
@@ -0,0 +1,7 @@
+import RestaurantFormDialog from './RestaurantFormDialog'
+import MenuTable from './MenuTable'
+
+export {
+  RestaurantFormDialog,
+  MenuTable
+}
---
+++ src/views/restaurant/detail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app-container">
+    <el-tabs tab-position="right" @tab-click="handleTabClick">
+      <el-tab-pane label="基础信息">用户管理</el-tab-pane>
+      <el-tab-pane
+              label="单品列表"
+              name="menu-table-tab"
+      >
+        <menu-table ref="menuTable" :restaurant-id="restaurantId" ></menu-table>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+  import {MenuTable} from './component'
+  export default {
+    name: "RestaurantDetail",
+    components: {
+      MenuTable
+    },
+    computed: {
+      restaurantId() {
+        return this.$route.params['id']
+      }
+    },
+    methods: {
+      handleTabClick(tab, event) {
+        if(tab.name === 'menu-table-tab') {
+          console.log(this.$refs)
+          this.$refs['menuTable'].getList()
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
