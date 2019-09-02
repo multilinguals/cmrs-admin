a694b9200a26bbcc7e42bcecd0d2f2a9b30fa682
sidfate <15757176627@163.com>
2019-09-02 14:57:36 +0800

    :rocket: change deploy public path.

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : 'https://multilinguals.github.io/cmrs-clinet-page/page',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : 'https://multilinguals.github.io/cmrs-admin-page/page',
   outputDir: 'dist/page',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
